6cf8557655043c2289b114c2e8c3624c23612550
Ian Meinert <marinesct@gmail.com>
2025-01-26 11:01:24 -0600

    svelte scaffolding

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="header">
+    <h1>Household Budgeting</h1>
+    <h2>Managing a budget with multiple incomes and banks</h2>
+    <p class="isa_info">
+        ***This is still under development.***
+    </p>
+    <div id="links">
+        | <a
+            href="https://github.com/ianmeinert/Household_Budget/issues"
+            target="_blank"
+            rel="noopener">Find a bug?</a
+        >
+        |
+        <a
+            href="https://cash.app/$PogLife4Me"
+            target="_blank"
+            rel="noopener"
+        >
+            <img src="/images/CashApp.jpg" alt="CashApp" width="15px" />
+            CashApp</a
+        > |
+    </div>
+</div>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 1024px;
+		min-height: 1250px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 300;
+	}
+
+	h2 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	#header {
+		padding-bottom: 25px;
+	}
+
+	#links {
+		float: right;
+	}
+
+	img {
+		border-radius: 10px 10px 10px 10px;
+	}
+
+	.isa_info {
+		color: #00529b;
+		background-color: #bde5f8;
+		margin: 10px 0px;
+		padding: 12px;
+	}
+</style>
